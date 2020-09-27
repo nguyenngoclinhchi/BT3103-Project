cc9e403ac3a6446ddf3c7c9f2994a5bd3291aba9
nguyenngoclinhchi <nguyenngoclinhchi@u.nus.edu>
2020-09-27 11:09:48 +0800

    Add Git deployment for Vue

--- vue.config.js
+++ vue.config.js
@@ -19,5 +19,8 @@ module.exports = {
     css: {
         // Enable CSS source maps.
         sourceMap: process.env.NODE_ENV !== 'production'
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/BT3103-Project/'
+        : '/'
 };
